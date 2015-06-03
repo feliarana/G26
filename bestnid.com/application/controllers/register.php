cc602d899c930856540486f01ebe9b3a1681c331
Emi <emiliano_retamar@hotmail.com.ar>
2015-06-03 18:02:57 -0300

    Regristro andando, si modificacion de la vista

--- bestnid.com/application/controllers/register.php
+++ bestnid.com/application/controllers/register.php
@@ -1,20 +1,20 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Register extends CI_Controller{
-	function __construct(){
+class Register extends CI_Controller {
+	function __construct() {
 		parent::__construct();
 		$this->load->helper('form');
 		$this->load->helper('url');
-		$this->load->model('user_model');
 		$this->load->library('session');
+		$this->load->model('user_model');
 	}
 
-	function index (){
-		$this->load->view("user_view/register_view");
+	function index () {
+		$this->load->view("register_view");
 	}
 
-	function verificarDatos(){ //AGREGAR QUE VERIFIQUE QUE NO QUEDE CAMPOS VACIOS
-		$datos= array(
+	function verificarDatos() { // AGREGAR QUE VERIFIQUE QUE NO QUEDE CAMPOS VACIOS
+		$datos = array(
 			'DNI' => $this->input->post('DNI'), 
 			'nombre' => $this->input->post('nombre'), 
 			'apellido' => $this->input->post('apellido'),
@@ -22,21 +22,17 @@ class Register extends CI_Controller{
 			'email' => $this->input->post('email'),  
 			'direccion' => $this->input->post('direccion'),
 			'telefono' => $this->input->post('telefono'),
-			'userAdmin' => 0	
+			'userAdmin' => 0
 			);
-		$query = $this->user_model->verificar_email($datos['email']);  // me guardo la query para tener los datos del usuario a lmacenar
-		$usuario = $query->result();
-		if ($query->num_rows == 0 ){ //Si devuelve nada, se registra al usuario , se crea la sesion y se carga la vista correspondiente.
-			$this->user_model->agregar_usuario($datos); //Apartir de aca para abajo, se crea la sesion
-			$user = array(
-				'email'=> $datos['email'],
-				'nombre'=> $datos['nombre'],
-				'apellido'=> $datos['apellido'],
-				'login'=> true
-				);
-
+		$query = $this->user_model->verificar_email($datos['email']);
+		if($query) { // Si devuelve true, se registra al usuario , se crea la sesion y se carga la vista correspondiente
+			$this->user_model->agregar_usuario($datos);
+			$user = array('email' => $datos['email'],
+				'nombre' => $datos['nombre'],
+				'apellido' => $datos['apellido'],
+				'login' => true);
 			$this->session->set_userdata($user);
-			$this->load->view('index_view'); //ACA IRÍA LA PAGINA PRINCIPAL DE BESTNID.
+			$this->load->view('index_view'); // ACA IRIA LA PAGINA PRINCIPAL DE BESTNID
 		}
 		else
 			echo "El email ya se encuentra registrado.";
