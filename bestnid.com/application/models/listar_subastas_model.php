dded51012ad383ca3ba2f7b130850473804502a4
Emi <emiliano_retamar@hotmail.com.ar>
2015-06-07 17:11:13 -0300

    Se traen las subastas vigentes de la Base de Datos

--- bestnid.com/application/models/listar_subastas_model.php
+++ bestnid.com/application/models/listar_subastas_model.php
@@ -5,10 +5,14 @@ class Listar_subastas_model extends CI_Model {
 	function __construct() {
 		parent::__construct(); 
 		$this->load->database();
+		$this->load->helper('date');
 	}
 
 	function obtenerSubastas() {
-		$this->db->order_by("fechaFin", "asc"); // Ordena las subastas de forma ascendente (por fecha de vencimiento, de las que estas mas proximas a vencer a las que estan mas lejanas a vencer)
+		$formato = "%Y-%m-%d";
+		$fechaActual = mdate($formato); // mdate retorna la fecha actual con el formato especificado
+		$this->db->where('fechaFin >', $fechaActual);
+		$this->db->order_by('fechaFin', 'asc'); // Ordena las subastas de forma ascendente (por fecha de vencimiento, de las que estas mas proximas a vencer a las que estan mas lejanas a vencer)
 		$query = $this->db->get('subasta'); // En $query almacena el resultado de la consulta
 		if($query->num_rows() > 0)
 			return ($query);
