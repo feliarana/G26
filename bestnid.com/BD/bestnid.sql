a9f56bd481ebd2df42c84c5d85ef4e921a13e1bc
felipe arana <felipearana10@hotmail.com>
2015-06-08 19:40:04 -0300

    Agregada las imagenes y actualizada la base con los nombres de estas

--- bestnid.com/BD/bestnid.sql
+++ bestnid.com/BD/bestnid.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 05-06-2015 a las 22:30:34
+-- Tiempo de generación: 09-06-2015 a las 00:34:59
 -- Versión del servidor: 5.6.24
 -- Versión de PHP: 5.6.8
 
@@ -85,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `subasta` (
   `fechaFin` date NOT NULL,
   `ganador` int(10) unsigned DEFAULT NULL,
   `nombreImagen` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `subasta`
@@ -97,7 +97,14 @@ INSERT INTO `subasta` (`idSubasta`, `nombre`, `descripcion`, `idUsuario`, `idCat
 (21, 'Silla', 'Silla donde se sentó Freddie Mercury una vez', 6, NULL, '2015-06-03', '2015-06-07', NULL, 'silla.jpg'),
 (22, 'Aceite y vinagre', '200ml de aceite y 300ml de vinagre. No incluye fascos', 8, NULL, '2015-06-05', '2015-06-15', NULL, 'aceitevinagre.jpg'),
 (25, 'Llama ', 'Llama adulta oriunda de Tilcara. Es mansita', 10, NULL, '2015-06-04', '2015-07-03', NULL, 'llama.jpg'),
-(26, 'Guante ', 'Guante de malla de acero inox. tejido, anticorte, marca *manulatex* de industria francesa', 10, NULL, '2015-06-03', '2015-07-01', NULL, 'guante.jpg');
+(26, 'Guante ', 'Guante de malla de acero inox. tejido, anticorte, marca *manulatex* de industria francesa', 10, NULL, '2015-06-03', '2015-07-01', NULL, 'guante.jpg'),
+(27, 'Casa con faro', 'Bonita casa con vista al mar y un faro muy luminoso para orientar barquitos', 12, NULL, '2015-06-04', '2015-06-30', NULL, 'faro.jpg'),
+(28, 'Cama', 'Cama de roble. Dos plazas.', 6, NULL, '2015-06-03', '2015-06-30', NULL, 'cama.jpg'),
+(29, 'Citroen 3CV', 'Modelo 79. Está como nuevo.', 1, NULL, '2015-05-07', '2015-06-01', NULL, 'citroen.jpg'),
+(34, 'Mochila de tela', 'Marca Mochilona. Medidas 60x40x30.', 8, NULL, '2015-05-28', '2015-06-15', NULL, 'mochila.jpg'),
+(35, 'Llave', 'Llave de aleación. Me la encontré en la vereda.', 12, NULL, '2015-05-24', '2015-06-21', NULL, 'llave.jpg'),
+(36, 'Varita mágica', 'Hecha de madera de sauco con alma de pluma de fénix. Perteneció a Albus Dumbledore.', 8, NULL, '2015-06-01', '2015-06-15', NULL, 'varita.jpg'),
+(37, 'Libro raro', 'No tiene nombre en la tapa. No entiendo que dicen los textos. Parece viejo.', 5, NULL, '2015-05-17', '2015-06-08', NULL, 'libro.jpg');
 
 -- --------------------------------------------------------
 
@@ -115,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `direccion` varchar(50) NOT NULL,
   `telefono` int(20) unsigned NOT NULL,
   `userAdmin` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
@@ -126,7 +133,9 @@ INSERT INTO `usuario` (`idUsuario`, `DNI`, `nombre`, `apellido`, `email`, `passw
 (5, 4567788, 'Emi', 'Retamar', 'hola@hotmail.com', '1231435', 'sakdjkld', 12314, 0),
 (6, 2132133, 'deh', 'dah', 'di@hotmail.com', '123', 'kasjdkla', 123, 0),
 (8, 0, '', '', '', '', '', 0, 0),
-(10, 34994585, 'Deh', 'Dah', 'askld|@gmail.com', '123', 'fuck', 14677, 0);
+(10, 34994585, 'Deh', 'Dah', 'askld|@gmail.com', '123', 'fuck', 14677, 0),
+(12, 33333333, 'fa', 'fa', 'f@f', '123456', 'fa', 0, 0),
+(13, 11111111, 'aa', 'aa', 'a@a.com', '123456', 'a', 12345678, 0);
 
 --
 -- Índices para tablas volcadas
@@ -185,12 +194,12 @@ MODIFY `idOferta` int(10) unsigned NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT de la tabla `subasta`
 --
 ALTER TABLE `subasta`
-MODIFY `idSubasta` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
+MODIFY `idSubasta` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=38;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-MODIFY `idUsuario` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+MODIFY `idUsuario` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- Restricciones para tablas volcadas
 --
