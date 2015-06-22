12917740106799d4bef59ab703782e250bd8f673
Emi <emiliano_retamar@hotmail.com.ar>
2015-06-22 04:36:45 -0300

    Rama actualizada

---
+++ bestnid.com/application/controllers/login.php
@@ -0,0 +1,51 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->helper('html');
+		$this->load->helper('url');
+		$this->load->library('session');
+		$this->load->model('login_model');
+		$this->load->model('listar_subastas_model');
+	}
+
+	function index() {
+		$this->load->view('login_view');
+	}
+
+	function recibirDatos() {
+		// Obtengo los datos ingresados en los inputs mediante el metodo post
+		$datos = array('email' => $this->input->post('email'),
+			'password' => $this->input->post('password'));
+		/* Se realiza un llamado a una funcion del model para obtener la consulta,
+		solo con el email es suficiente para conseguir los datos del usuario ya que
+		este campo es unico y no puede repetirse */
+		$query = $this->login_model->buscarUsuario($datos['email']); // La consulta retorna un objeto de tipo query lo cual tiene sus propias funciones o metodos
+		if($query) {
+			/* Obtengo la tupla correspondiente al email del usuario en cuestion (como el email no puede repetirse, la consulta solo retorna una sola tupla) */
+			$usuario = $query->result(); // result() es una funcion del objeto query que devuelve un arreglo de tuplas (en este caso devuelve una sola tupla)
+			if($usuario[0]->password == $datos['password']) { // $usuario[0] accede a la primera tupla del arreglo y ->password accede al valor que se encuentra en el campo password
+				$user = array('email' => $usuario[0]->email,
+					'nombre' => $usuario[0]->nombre,
+					'apellido' => $usuario[0]->apellido,
+					'id' => $usuario[0]->idUsuario,
+					'login' => true);
+				$this->session->set_userdata($user);
+				redirect(base_url(index_page().'/index'));
+			}
+			else {
+				$mensaje['datos_error'] = 'Los datos ingresados son incorrectos';
+				$this->load->view('login_view', $mensaje);
+			}
+		}
+		else {
+			$mensaje['datos_error'] = 'Los datos ingresados son incorrectos';
+			$this->load->view('login_view', $mensaje);	
+		}
+	}
+}
+
+?>
---
+++ bestnid.com/index_view.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    	<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<!-- Se cargan los estilos de bootstrap -->
+    	<link href="<?= base_url('css/bootstrap.min.css') ?>" rel="stylesheet" media="screen">
+    	<!-- Se cargan los estilos de la libreria dataTables -->
+    	<link href="<?= base_url('css/jquery.dataTables.min.css') ?>" rel="stylesheet" type="text/css">
+    	<!-- Se cargan los estilos con bootstrap de la libreria dataTables -->
+    	<link href="<?= base_url('css/dataTables.bootstrap.css') ?>" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<!-- En total la tabla debe sumar 12 columnas -->
+		<div class="row">
+			<div class="col-md-3">
+			</div>
+			<div class="col-md-1">
+  				<a href="<?=base_url('images/enConstruccion.jpg') ?>" />Categorías</a> <!-- Se ubica en columna 4 -->
+  			</div>
+  			<div class="col-md-1">
+				<a href="<?=base_url('images/enConstruccion.jpg') ?>">Ayuda</a> <!-- Se ubica en columna 5 -->
+			</div>
+			<div class="col-md-2">
+				<!-- Se ubica en columna 6 -->
+				<p align="center">
+					<img src="<?= base_url('images/logo.png') ?>">
+				</p>
+			</div>
+			<div class="col-md-1"> <!-- Se ubica en columna 8 -->
+				<a href="<?= base_url('/index.php/register') ?>">Registrarse</a>
+			</div>
+			<div class="col-md-2"> <!-- Se ubica en columna 9 -->
+				<a href="<?= base_url('/index.php/login') ?>">Iniciar Sesión</a>
+			</div>
+			<div class="col-md-2">
+			</div>
+		</div>
+		<h1 align="center">Bestnid</h1>
+		<h2 align="center">Elegí con el corazón</h2>
+		<br>
+		<!-- Estos archivos deben cargarse si o si antes de definir la tabla, sino no los toma -->
+		<!-- Se carga jquery -->
+    	<script src="<?= base_url('js/jquery.js') ?>" type="text/javascript" charset="utf8"></script>
+		<!-- Se carga la libreria dataTables -->
+    	<script src="<?= base_url('js/jquery.dataTables.min.js') ?>" type="text/javascript" charset="utf8"></script>
+		<table cellpadding="0" cellspacing="0" border="0" class="display" id="tablaSubastas">
+			<thead>
+				<tr>
+					<th>Imagen</th>
+					<th>Nombre</th>
+					<th>Descripción</th>
+					<th>Fecha de Finalización</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+					if($subastas) {
+						foreach($subastas->result() as $subasta) { ?>
+							<tr class="gradeX">
+								<td>
+									<center> <img src="<?= base_url('images/'.$subasta->nombreImagen); ?>" width="50px" height="50" > </center>
+								</td>
+								<td>
+									<center> <?= $subasta->nombre ?> </center>
+								</td>
+								<td> 
+									<center> <?= $subasta->descripcion ?> </center>
+								</td>
+								<td>
+									<center> <?= $subasta->fechaFin ?> </center>
+								</td>
+							</tr>
+				<?php
+						}
+					}
+				?>
+			</tbody>
+		</table>
+		<!-- Configuracion de la dataTable -->
+		<script type="text/javascript" charset="utf-8">
+			$(document).ready(function() {
+				$('#tablaSubastas').dataTable( {
+					"aaSorting":[],
+        			"language": {
+        				"search": "Buscar",
+            			"lengthMenu": "Mostrar _MENU_ subastas por página",
+            			"zeroRecords": "No se han encontrado subastas",
+            			"info": "Mostrando página _PAGE_ de _PAGES_",
+            			"infoEmpty": "No hay registros disponibles",
+            			"infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            			"loadingRecords": "Cargando",
+            			"processing":     "Procesando",
+            			"zeroRecords":    "No hay subastas coincidentes encontradas",
+            			"paginate": {
+        					"first":      "Primero",
+        					"last":       "Ultimo",
+        					"next":       "Siguiente",
+        					"previous":   "Anterior"
+    					},
+    					"aria": {
+        					"sortAscending":  ": activar para ordenar columna de forma ascendente",
+        					"sortDescending": ": activar para ordenar columna de forma descendente"
+    					}
+        			}
+    			} );
+			} );
+		</script>
+		<!-- Se cargan las funciones javascript de Bootstrap -->
+    	<script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+    	<!-- Este archivo le da estilo introduciendole bootstrap a la libreria dataTables mediante javascript -->
+    	<script src="<?= base_url('js/dataTables.bootstrap.min.js') ?>" type="text/javascript" charset="utf8"></script>
+	</body>
+</html>
