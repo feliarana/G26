859a32b16f66064eca711df2a5332ee41012686c
Felipe Arana <felipearana10@hotmail.com>
2015-06-22 05:49:01 -0300

    Listo para trabajar en mi branch

--- bestnid.com/application/views/categorias_view.php
+++ bestnid.com/application/views/categorias_view.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -58,4 +59,66 @@
         <!-- Este archivo le da estilo introduciendole bootstrap a la libreria dataTables mediante javascript -->
         <script src="<?= base_url('js/dataTables.bootstrap.min.js') ?>" type="text/javascript" charset="utf8"></script>
     </body>
+=======
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    	<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<!-- Se cargan los estilos de bootstrap -->
+        <link href="<?= base_url('css/bootstrap.min.css') ?>" rel="stylesheet" media="screen">
+	</head>
+    <body>
+       <p align="center">
+            <a href="<?= base_url(index_page().'/index') ?>">
+                <img src="<?= base_url('images/logo.png') ?>">
+            </a>
+        </p>
+        <h1 align="center">
+            Categorias
+        </h1>
+        <br>
+        <br>
+        <div table-responsive>
+            <table class="table table-condensed">
+                <?php
+                $i = 0;
+                foreach($categorias->result() as $categoria) {
+                    if($i == 0) { ?>
+                        <tr>
+                    <?php
+                        $i = 3;
+                    }
+                    ?>
+                    <td>
+                        <p align="center"> 
+                            <a href="<?= base_url(index_page().'/categorias/listado?id='.$categoria->idCategoria) ?>">
+                                <img src="<?= base_url('images/'.$categoria->nombreImagen) ?>" class="img-rounded" width="300" height="200">
+                                <p align="center" > 
+                                    <label>
+                                        <?= $categoria->nombreCategoria ?>
+                                    </label>
+                                </p>
+                            </a>
+                        </p>
+                    </td>
+                    <?php
+                    $i--;
+                    if($i == 0) { ?>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                <?php
+                }
+                ?>
+            </table>
+        </div> 
+        <!-- Se cargan las funciones javascript de Bootstrap -->
+        <script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+        <!-- Este archivo le da estilo introduciendole bootstrap a la libreria dataTables mediante javascript -->
+        <script src="<?= base_url('js/dataTables.bootstrap.min.js') ?>" type="text/javascript" charset="utf8"></script>
+    </body>
+>>>>>>> emiBranch
 </html>
