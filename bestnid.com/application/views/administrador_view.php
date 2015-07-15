5296f8245c6f0d2d208ec3c388661b8b0b2004d4
Emi <emiliano_retamar@hotmail.com.ar>
2015-07-15 00:12:07 -0300

    Ver los usuarios registrados entre 2 fechas terminado, el unico detalle es que no me carga los estilos de bootstrap en la tabla

--- bestnid.com/application/views/administrador_view.php
+++ bestnid.com/application/views/administrador_view.php
@@ -8,6 +8,12 @@
     	<link href="<?= base_url('css/bootstrap.min.css') ?>" rel="stylesheet" media="screen">
     	<!-- Se cargan los estilos para la sidebar -->
     	<link href="<?= base_url('css/simple-sidebar.css') ?>" rel="stylesheet" media="screen">
+        <!-- Se cargan los estilos de la libreria dataTables -->
+        <link href="<?= base_url('css/jquery.dataTables.min.css') ?>" rel="stylesheet" type="text/css">
+        <!-- Se cargan los estilos con bootstrap de la libreria dataTables -->
+        <link href="<?= base_url('css/dataTables.bootstrap.css') ?>" rel="stylesheet" type="text/css">
+        <!-- JQuery cargado de forma local (sin conexion a internet) -->
+        <script src="<?= base_url('js/jquery.js') ?>"></script>
         <title> Administrador </title>   
     </head>
 	<body>
@@ -81,6 +87,88 @@
                                     <h3 align="center">
                                         <?= 'Usuarios registrados entre el '.date('d-m-Y', strtotime($fecha1)).' y el '.date('d-m-Y', strtotime($fecha2)) ?>
                                     </h3>
+                                    <br>
+                                    <br>
+                                    <!-- Se carga la libreria dataTables -->
+                                    <script src="<?= base_url('js/jquery.dataTables.min.js') ?>" type="text/javascript" charset="utf8"></script>
+                                    <table cellpadding="0" cellspacing="0" border="0" class="display" id="tablaSubastas">
+                                        <thead>
+                                            <tr>
+                                                <th>Nombre y Apellido</th>
+                                                <th>Email</th>
+                                                <th>Estado</th>
+                                                <th>Fecha de Registro</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                                if($usuariosRegistrados) {
+                                                    foreach($usuariosRegistrados->result() as $usuario) { ?>
+                                                        <tr class="gradeX">
+                                                            <td>
+                                                                <center>
+                                                                    <?= $usuario->nombre.' '.$usuario->apellido ?>
+                                                                </center>
+                                                            </td>
+                                                            <td>
+                                                                <center>
+                                                                    <?= $usuario->email ?>    
+                                                                </center>
+                                                            </td>
+                                                            <td> 
+                                                                <center>
+                                                                    <?php 
+                                                                        if($usuario->activo) {
+                                                                            echo 'Activo';
+                                                                        }
+                                                                        else {
+                                                                            echo 'No activo';
+                                                                        }
+                                                                    ?>
+                                                                </center>
+                                                            </td>
+                                                            <td>
+                                                                <center>
+                                                                    <?= date('d-m-Y', strtotime($usuario->fechaRegistro)) ?>
+                                                                </center>
+                                                            </td>
+                                                        </tr>
+                                            <?php
+                                                    }
+                                                }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                    <!-- Configuracion de la dataTable -->
+                                    <script type="text/javascript" charset="utf-8">
+                                        $(document).ready(function() {
+                                            $('#tablaSubastas').dataTable( {
+                                                "aaSorting":[],
+                                                "aoColumnDefs":[ { 'bSortable': false } ],
+                                                "language": {
+                                                    "search": "Buscar",
+                                                    "lengthMenu": "Mostrar _MENU_ subastas por página",
+                                                    "zeroRecords": "No se han encontrado subastas",
+                                                    "info": "Mostrando página _PAGE_ de _PAGES_",
+                                                    "infoEmpty": "No hay registros disponibles",
+                                                    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                                                    "loadingRecords": "Cargando",
+                                                    "processing":     "Procesando",
+                                                    "zeroRecords":    "No hay subastas coincidentes encontradas",
+                                                    "paginate": {
+                                                        "first":      "Primero",
+                                                        "last":       "Ultimo",
+                                                        "next":       "Siguiente",
+                                                        "previous":   "Anterior"
+                                                    },
+                                                    "aria": {
+                                                        "sortAscending":  ": activar para ordenar columna de forma ascendente",
+                                                        "sortDescending": ": activar para ordenar columna de forma descendente"
+                                                    }
+                                                }
+                                            } );
+                                        } );
+                                    </script>
                     <?php
                                     break;
                                 case 'value': 
@@ -213,9 +301,6 @@
             </div>
             <!-- /#page-content-wrapper -->
         </div>
-
-		<!-- JQuery cargado de forma local (sin conexion a internet) -->
-    	<script src="<?= base_url('js/jquery.js') ?>"></script>
 		<!-- Se cargan las funciones javascript de Bootstrap -->
     	<script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
 	</body>
