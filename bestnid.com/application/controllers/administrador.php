9a8beb577bab4d988f4e51498e5e9c5f2d7acf38
Gaston Alvarez <gastonaltr@gmail.com>
2015-07-17 19:54:00 -0300

    Corregido para que no se puedan cargar dos categorías con el mismo nombre.

---
+++ bestnid.com/application/controllers/administrador.php
@@ -0,0 +1,111 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Administrador extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->helper('url');
+		$this->load->helper('date');
+		$this->load->library('session');
+		$this->load->model('categorias_model');
+		$this->load->model('administrador_model');
+	}
+
+	function index() {
+		if ($this->session->userdata('userAdmin'))
+			$this->load->view('administrador_view');
+		else
+			redirect(base_url(index_page().'/index'));	
+	}
+
+	function consultar_usuarios() {
+		$datos['opcion'] = 'consultar_usuarios';
+		$datos['fechaActual'] = mdate('%Y-%m-%d');
+		$this->load->view('administrador_view', $datos);
+	}
+
+	function usuarios_registrados() {
+		$datos['fecha1'] = $this->input->post('fecha1');
+		$datos['fecha2'] = $this->input->post('fecha2');
+		if($datos['fecha1'] <= $datos['fecha2']) {
+			$datos['opcion'] = 'usuarios_registrados';
+			$datos['usuariosRegistrados'] = $this->administrador_model->obtenerUsuariosRegistrados($datos['fecha1'], $datos['fecha2']);
+			$this->load->view('administrador_view', $datos);
+		}
+		else {
+			$datos['opcion'] = 'consultar_usuarios';
+			$datos['fechaActual'] = mdate('%Y-%m-%d');
+			$datos['error'] = 'Los datos ingresados son incorrectos. La primera fecha debe ser menor o igual a la segunda';
+			$this->load->view('administrador_view', $datos);
+		}
+	}
+
+	function consultar_subastas() {
+		$datos['opcion'] = 'consultar_subastas';
+		$datos['fechaActual'] = mdate('%Y-%m-%d');
+		$this->load->view('administrador_view', $datos);
+	}
+
+	function subastas_vendidas() {
+		$datos['fecha1'] = $this->input->post('fecha1');
+		$datos['fecha2'] = $this->input->post('fecha2');
+		if($datos['fecha1'] <= $datos['fecha2']) {
+			$datos['opcion'] = 'subastas_vendidas';
+			$datos['subastadores'] = $this->administrador_model->obtenerSubastasVendidas($datos['fecha1'], $datos['fecha2']);
+			$datos['ganadores'] = $this->administrador_model->obtenerOfertasGanadoras($datos['fecha1'], $datos['fecha2']);
+			$this->load->view('administrador_view', $datos);
+		}
+		else {
+			$datos['opcion'] = 'consultar_subastas';
+			$datos['fechaActual'] = mdate('%Y-%m-%d');
+			$datos['error'] = 'Los datos ingresados son incorrectos. La primera fecha debe ser menor o igual a la segunda';
+			$this->load->view('administrador_view', $datos);
+		}
+	}
+
+	function crear_categoria() {
+		$datos['opcion'] = 'crear_categoria';
+		$this->load->view('administrador_view', $datos);
+	}
+
+	function agregarCategoria() {
+		$datos['nombreCategoria'] = $this->input->post('nombreCategoria');
+		$datos['nombreImagen'] = date('dmYHis').'.jpg';
+		$config['upload_path'] = FCPATH.'images';
+		$config['allowed_types'] = 'gif|jpg|png|jpeg';
+		$config['max_size']	= 10*1024;
+		$config['max_width']  = '5000';
+		$config['max_height']  = '5000';
+		$config['file_name']  = $datos['nombreImagen'];
+		$this->load->library('upload', $config);
+
+		if($this->administrador_model->obtenerCategoriaPorNombre($datos) && $this->upload->do_upload()) {
+			$this->administrador_model->agregarCategoria($datos);
+			print "<script type=\"text/javascript\">alert('Categoria agregada con exito!');</script>"; 
+    		$this->load->view('administrador_view');
+		}
+		else {
+			print "<script type=\"text/javascript\">alert('Nombre de categoría o archivo no permitido. Por favor, elija uno válido.');</script>"; 
+			$datos['opcion'] = 'crear_categoria';
+			$this->load->view('administrador_view', $datos);
+		}
+	}
+
+	function eliminar_categoria() {
+		$datos['opcion'] = 'eliminar_categoria';
+		$datos['categorias'] = $this->categorias_model->obtenerCategorias();
+		if($datos['categorias']) {
+			$this->load->view('administrador_view', $datos);
+		}
+	}
+
+	function eliminarDatosCategoria() {
+		$idCategoria = $this->input->get('idCategoria');
+		$this->administrador_model->eliminarCategoria($idCategoria);
+		redirect(base_url(index_page().'/administrador'));
+	}
+
+}
+
+?>
--- bestnid.com/application/models/administrador_model.php
+++ bestnid.com/application/models/administrador_model.php
@@ -56,6 +56,20 @@ class Administrador_model extends CI_Model {
 		$this->db->insert('categoria', $categoria);
 	}
 
+	function obtenerCategoriaPorNombre($categoria) {
+		//Consulta para que no se repitan los nombres de las categorias
+		$this->db->from('categoria');
+		$this->db->where('nombreCategoria', $categoria['nombreCategoria']);
+		$query = $this->db->get();
+
+		if($query->num_rows() == 0) {
+			return (true);
+		}
+		else {
+			return (false);
+		}
+	}
+
 	function eliminarCategoria($idCategoria) {
 		$this->db->where('idCategoria', $idCategoria);
 		$this->db->update('categoria', array('eliminada' => true));
